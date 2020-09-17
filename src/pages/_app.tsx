1cb9cf6de2d0186f85cfab83e7921abd79187f18
retn0 <nbsp1221@gmail.com>
2020-09-18 02:41:59 +0900

    🔨 Create Material-UI settings for Next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
+
+export default function MyApp(props: AppProps): JSX.Element {
+  const {
+    Component,
+    pageProps
+  } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    // Remove the server-side injected CSS.
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <title>Streaming Site with TypeScript</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
