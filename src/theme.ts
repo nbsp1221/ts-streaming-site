960774cd058a58109228a19b4ef602351b5414ba
retn0 <nbsp1221@gmail.com>
2020-09-19 03:07:19 +0900

    🔨 Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-  
+  palette: {
+    type: 'dark',
+    background: {
+      default: '#1b1b1b',
+      paper: '#282828'
+    },
+    primary: {
+      light: '#a6d4fa',
+      main: '#90caf9',
+      dark: '#648dae'
+    },
+    secondary: {
+      light: '#f6a5c0',
+      main: '#f48fb1',
+      dark: '#aa647b'
+    }
+  }
 });
 
 export default theme;
